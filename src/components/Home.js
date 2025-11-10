754e2adedfe7ffee12613a7b3cb3459bd8e496ac
awhite0123 <loniluv09@gmail.com>
2025-11-10 10:12:01 -0500

    added homepage text

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,8 +20,16 @@ function TextBlock1(){
     return ( 
         <div className='homepageText1'> 
             <p style={{ textAlign: "left" }}> Welcome to Safe-AI Initiative! </p>
-            <p style={{ textAlign: "left" }}> Here, our goal is to raise awareness about some of the explored downsides to AI usage. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class </p>
-            <p style={{ textAlign: "left" }}> aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. bus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class </p>
+            <h3> Our goal </h3>
+            <p style={{ textAlign: "left" }}> Our goal is to provide users with an ample amount of information about AI 	and potential issues that are arising with its growing usage. We hope this raises awareness and provides resources for some of the lesser explored downsides of AI usage and development. </p>
+            <h3> Our approach </h3>
+            <p style={{ textAlign: "left" }}> On our page we hope to provide education on how our increasing AI usage can have negative effects on the environment, mental health, and society. We have done this through providing easy to understand, interactive, and visual information.  </p>
+            <p style={{ textAlign: "left" }}> We don’t want this to be a page filled with negativity but rather realistic education. To show a light at the end of the tunnel, we have provided alternate resources for AI and some ways to get involved/advocate for safer AI usage!  </p>
+            <h3> What is AI? </h3>
+            <p style={{ textAlign: "left" }}> Before we dive into the downsides, we want to provide some simple definitions so that there is a general understanding of what AI is. </p>
+            <p style={{ textAlign: "left" }}> AI is a blanket term that encompasses a variety of technologies that enable computers (machines) to mimic human intelligence and decision making. </p>
+            <p style={{ textAlign: "left" }}> Here, our focus is on AI that is most commonly used. Think. chatbots like chatgpt, customer service bots, and other ML technologies of the sort </p>
+            <p style={{ textAlign: "left" }}> Now that there is a general understanding of what AI is and our focus, feel free to dig into the rest of the site! </p>
         </div>
     )
 }
