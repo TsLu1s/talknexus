a9f9b2e201440f4ad2e548ef7578518a07e70b74
awhite0123 <loniluv09@gmail.com>
2025-11-10 10:57:33 -0500

    webpage text updates

--- src/components/Environment.js
+++ src/components/Environment.js
@@ -15,13 +15,14 @@ function TextBlock2(){
     return ( 
         <div className='environmentText2'> 
             <h3 style={{ textAlign: "left" }}> Overview </h3>
-            <p style={{ textAlign: "left" }}> Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class </p>
+            <p style={{ textAlign: "left" }}> Most have heard that AI has negative impacts on the environment, but is there an understanding of how signifcant it is? </p>
             <h4 style={{ textAlign: "left" }}> Energy</h4>
-            <p style={{ textAlign: "left" }}> aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. bus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class </p>
+            <p style={{ textAlign: "left" }}> AI requires a huge amount of energy to train and maintain AI models. </p>
             <h4 style={{ textAlign: "left" }}> Water</h4>
-            <p style={{ textAlign: "left" }}> aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. bus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class </p>
+            <p style={{ textAlign: "left" }}> All major steps of the AI supply chain from data center cooling systems to semiconductor production compete with communities for the same municipal fresh water used for drinking.​ </p>
+            <p style={{ textAlign: "left" }}> AI's water consumption is measured in three categories according to Li et al. (2025):​ Scope 1: On-site​ Water drawn from source and evaporated through data center cooling systems​ Scope 2: Off-site​ Water drawn from source and evaporated during electricity generation​ Scope 3: Supply chain​ Water evaporated or otherwise consumed across chip manufacturing and other primary AI computing supply chain elements​ </p>
         </div>
-    )
+    );
 }
 
 export default Environment;
